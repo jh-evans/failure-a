0cbf7b7a759ae0b846e4c84475bf2c3eb12a495a
James Huw Evans <jhevansmailbox-github@yahoo.com>
2023-08-18 13:12:37 +0100

    Fifth use example

--- failure-a/src/main/java/org/darien/use/five/Main.java
+++ failure-a/src/main/java/org/darien/use/five/Main.java
@@ -13,13 +13,14 @@ import org.darien.types.*;
 import org.darien.types.impl.*;
 import org.darien.types.utils.FailureUtils;
 
+// write code to take away exceptions having to be thrown, to keep flow easier
+
 public class Main {
 	public S getFile(Path filename) {
 		if(FailureUtils.oneIsNull(filename)) {
 			return FailureUtils.theNull(filename);
 		}
-		
-		// write code to take away exceptions having to be thrown, to keep flow easier
+
 		File file = filename.toFile();
 		if(!file.exists()) {
 			return FailureUtils.theFalse(file.exists());
@@ -62,16 +63,64 @@ public class Main {
 	
 	    return new Success(resultStringBuilder.toString());
 	}
+	
+
+	public S getFileSlowly(Path filename) {
+		if(FailureUtils.oneIsNull(filename)) {
+			return FailureUtils.theNull(filename);
+		}
+		
+		// write code to take away exceptions having to be thrown, to keep flow easier
+		File file = filename.toFile();
+		if(!file.exists()) {
+			return FailureUtils.theFalse(file.exists());
+		}
+		
+		String line;
+		StringBuilder resultStringBuilder = new StringBuilder();
+	    try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+        	try {
+				Thread.sleep(10000); // If filename is on a removable media and during this delay
+			} catch (InterruptedException e) { // the media is removed, IOException is thrown
+				e.printStackTrace();
+			}
+	    	while ((line = br.readLine()) != null) {
+	            resultStringBuilder.append(line).append("\n");
+	        }
+	    } catch (IOException e) {
+			return new FExp(e);
+	    }
+	
+	    return new Success(resultStringBuilder.toString());
+	}
 
 	public static void main(String[] argv) {
 		Main m = new Main();
 		
-		Path inputfile = Paths.get(System.getProperty("user.dir"), "org.darian.use.five", "tamoshanter");
+		Path inputfile = Paths.get("E:", "pom.xml");
+
+		System.out.println("In");
+		S obj = m.getFileSlowly(inputfile);
+		System.out.println("Out");
 		
-		S obj = m.getFile(inputfile);
 		if(obj.eval()) {
 			String pom = (String) obj.unwrap();
-			System.out.println("Success"); // try printing pom
+			System.out.println("Success");
+		} else {
+			switch(obj) {
+			case FailureArgIsNull fain -> System.out.println(fain.getLocation());
+			case FailureArgIsFalse faif -> System.out.println(faif.getLocation());
+			case FailureException fe -> System.out.println(fe.getLocation());
+			default -> System.out.println("As written, cannot happen");
+			}
+		}
+		
+		inputfile = Paths.get(System.getProperty("user.dir"), "org.darian.use.five", "tamoshanter");
+		
+		obj = m.getFile(inputfile);
+		if(obj.eval()) {
+			String pom = (String) obj.unwrap();
+			System.out.println("Success");
 		} else {
 			switch(obj) {
 			case FailureArgIsNull fain -> System.out.println(fain.getLocation());
